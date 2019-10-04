34f423401a00bb614b9f7e967a1602593949e2ea
DidinSalahudin <didinsalahudin@gmail.com>
2019-10-04 09:56:37 +0700

    Updates

---
+++ precache-manifest.21d8b4f163bdaa782cc1d33ae85f6e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7060ae0b0be3497f13eb4cbe280ff00",
+    "url": "/react-app1/index.html"
+  },
+  {
+    "revision": "ac9edaaf18bded62e5a1",
+    "url": "/react-app1/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a8b75adb215a0aab535d",
+    "url": "/react-app1/static/js/2.2c04d281.chunk.js"
+  },
+  {
+    "revision": "ac9edaaf18bded62e5a1",
+    "url": "/react-app1/static/js/main.a8012d15.chunk.js"
+  },
+  {
+    "revision": "0538663b3decf6d0f8d3",
+    "url": "/react-app1/static/js/runtime-main.3a836930.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-app1/static/media/logo.25bf045c.svg"
+  }
+]);
